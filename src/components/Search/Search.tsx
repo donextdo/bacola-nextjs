9af7e8d77219f4fe1414add1f6d50b635490da05
Thisara Thathsarani <thisararpg@gmail.com>
2023-04-18 10:21:49 +0530

    change search function

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { FiSearch } from "react-icons/fi";
 import Spinner from "../Spinner/Spinner";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import { Product } from "./product";
+import { fetchProducts } from "@/features/product/productSlice";
 
 export const SearchItem = () => {
   const [searchItem, setSearchItem] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+
+  const dispatch = useDispatch();
+  const products = useSelector(
+    (state: RootState) => state.product.products
+  ) as Product[];
+  useEffect(() => {
+    dispatch(fetchProducts());
+    console.log("search data ", products);
+  }, [dispatch]);
 
   const handleSubmit = async (e: { preventDefault: () => void }) => {
     setIsLoading(true);
@@ -20,6 +33,10 @@ export const SearchItem = () => {
     //   console.log(data);
     //   alert("Search button clicked! " + searchItem);
     // }
+  };
+
+  const onSearch = (searchItem: string) => {
+    console.log("search == ", searchItem);
   };
 
   // useEffect(() => {
@@ -53,7 +70,7 @@ export const SearchItem = () => {
       ) : (
         <div
           className="bg-gray-200 rounded-br rounded-tr min-h-[60px] min-w-[60px]  flex items-center justify-center hover:cursor-pointer"
-          onClick={handleSubmit}
+          onClick={() => onSearch(searchItem)}
         >
           <FiSearch
             type="submit"
@@ -70,6 +87,7 @@ export const SearchItem = () => {
         <p>No results found.</p>
       )} */
       )}
+      <div className="bg-white flex flex-col border-solid border-2 border-gray-400 "></div>
     </div>
   );
 };
